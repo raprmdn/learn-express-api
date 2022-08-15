ab4b555dca99e1f7624810aea22eaf91a3fc73cf
raprmdn <rafiputraramadhan14@gmail.com>
2022-08-15 16:44:52 +0700

    create product, fastest-validator.

---
+++ routes/products.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let router = express.Router();
+const Validator = require('fastest-validator');
+
+const { Product } = require('../models');
+
+const v = new Validator();
+
+router.post('/', async (req, res) => {
+    const schema = {
+        name: 'string',
+        brand: 'string',
+        description: 'string|optional',
+    }
+
+    const validate = v.validate(req.body, schema);
+
+    if (validate.length) {
+        return res.status(400).json(validate);
+    }
+
+    const product = await Product.create(req.body);
+
+    res.json(product);
+})
+
+module.exports = router;
